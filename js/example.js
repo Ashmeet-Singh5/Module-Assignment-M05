149e21c8e25cd4a609f3f3ec4f1620a05e9108f9
Ashmeet-Singh5 <122590891+Ashmeet-Singh5@users.noreply.github.com>
2024-02-08 12:57:57 -0800

    Add files via upload

---
+++ js/example.js
@@ -0,0 +1,18 @@
+var table;             // Unit of table
+var operator = 'multiplication'; // Type of calculation
+var i = 1;                 // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>';              // Message
+
+table = prompt("Enter a Value:");
+
+if (operator == "multiplication") {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br>';
+    i++;
+  }
+}
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
